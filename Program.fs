ac229a97d1c3aef5a9bc892bc14a750a7ddb70ab
Zaid <zaid.naom@gmail.com>
2020-02-26 23:30:49 +0100

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿open Npgsql.FSharp
+open System
+
+let users (connectionString: string) =
+    connectionString
+    |> Sql.connect
+    |> Sql.query "SELECT * FROM users"
+    |> Sql.execute (fun read ->
+        {|
+            userId = read.int "user_id"
+            username = read.text "username"
+            lastLogin = read.dateTimeOrNone "last_login"
+        |})
